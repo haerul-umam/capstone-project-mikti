c685e27a25fc09f9c885686ed906da623849767a
haerul-umam <dummytgr2@gmail.com>
2024-05-28 20:32:09 +0700

    fix(database): load env file

--- app/database.go
+++ app/database.go
@@ -6,11 +6,18 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"github.com/joho/godotenv"
 )
 
 func InitConnetion() *gorm.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading env file")
+	}
+
 	dsn := os.Getenv("DSN")
 	configuration := gorm.Config{}
+
 	db, err := gorm.Open(postgres.Open(dsn), &configuration)
 	if err != nil {
 		log.Fatal(err.Error())
