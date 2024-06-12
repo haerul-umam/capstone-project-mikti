7688a68d6f07739c7c8738a808929b68332a2500
Yonatan <yonatan.pv20@gmail.com>
2024-06-12 19:49:58 +0700

    fix(category): fix untuk delete category dan category list

--- repository/category_repository_impl.go
+++ repository/category_repository_impl.go
@@ -28,7 +28,7 @@ func (repo *CategoryRepositoryImpl) CreateCategory(category domain.Category) (do
 func (repo *CategoryRepositoryImpl) GetCategories() ([]domain.Category, error) {
 	var categories []domain.Category
 
-	err := repo.db.Where("deleted_at TIMESTAMP").Find(&categories).Error
+	err := repo.db.Where("deleted_at IS NULL").Find(&categories).Error
 
 	if err != nil {
 		return []domain.Category{}, err
@@ -53,6 +53,14 @@ func (repo *CategoryRepositoryImpl) UpdateCategory(category domain.Category) (do
 }
 
 func (repo *CategoryRepositoryImpl) DeleteCategory(id int) error {
+	var category domain.Category
+	result := repo.db.First(&category, id)
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			return errors.New("category Sudah Di Hapus")
+		}
+		return result.Error
+	}
 	now := time.Now()
 	return repo.db.Model(&domain.Category{}).Where("id = ?", id).Update("deleted_at", &now).Error
 }
